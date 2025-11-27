679f25ec990125cf94398c438721855f8bd64958
Diogo <hello@diogo.fi>
2025-11-27 15:39:52 +0000

    services/pocket-id: setup litestream for backup

--- modules/nixos/services/pocket-id.nix
+++ modules/nixos/services/pocket-id.nix
@@ -25,6 +25,10 @@ in
       group = "pocket-id";
     };
 
+    sops.secrets.litestream = {
+      sopsFile = "${self}/secrets/services/litestream.yaml";
+    };
+
     services = {
       pocket-id = {
         enable = true;
@@ -41,6 +45,28 @@ in
         };
 
         environmentFile = config.sops.secrets.pocket-id.path;
+      };
+
+      litestream = {
+        enable = true;
+
+        settings = {
+          db = [
+            {
+              path = "/var/lib/pocket-id/data/pocket-id.db";
+              replicas = [
+                {
+                  type = "s3";
+                  bucket = "dahlia";
+                  path = "pocket-id/pocket-id.db";
+                  endpoint = "s3.gra.io.cloud.ovh.net";
+                }
+              ];
+            }
+          ];
+        };
+
+        environmentFile = config.sops.secrets.litestream.path;
       };
 
       caddy.virtualHosts.${cfg.domain}.extraConfig = ''reverse_proxy ${cfg.host}:${toString cfg.port}'';
