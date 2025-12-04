1319fdd472e4b71500f63fea9023db2c2ca1db64
Diogo <hello@diogocastro.net>
2025-12-04 16:36:39 +0000

    services/postgresql: fix auth

--- modules/nixos/services/postgresql.nix
+++ modules/nixos/services/postgresql.nix
@@ -39,6 +39,15 @@ in
       checkConfig = true;
       enableTCPIP = true;
 
+      authentication = ''
+        local all all peer
+        host all all 127.0.0.1/32 md5
+        host all all ::1/128 md5
+
+        host all all 100.100.10.0/24 md5
+        host pocketid pocketid 100.100.20.0/24 md5
+      '';
+
       settings = {
         port = cfg.port;
 
